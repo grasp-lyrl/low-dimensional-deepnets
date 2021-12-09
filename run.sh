11a92874dc8d2fdf5b73deeefb2df82057d58dff
Jialin Mao <jmao@sas.upenn.edu>
2021-12-09 03:47:20 +0000

    new plot

--- run.sh
+++ run.sh
@@ -1,34 +1,4 @@
 #!/bin/bash
-python runner.py --seed 92 --numc 4 --widen 4 --relabel 0.0
-sleep 30
-python runner.py --seed 92 --numc 4 --widen 4 --relabel 0.1
-sleep 30
-python runner.py --seed 92 --numc 4 --widen 4 --relabel 0.2
-sleep 30
-python runner.py --seed 92 --numc 4 --widen 4 --relabel 0.5
-sleep 30
-python runner.py --seed 92 --numc 4 --widen 4 --relabel 0.9
-sleep 30
-python runner.py --seed 102 --numc 4 --widen 4 --relabel 0.0
-sleep 30
-python runner.py --seed 102 --numc 4 --widen 4 --relabel 0.1
-sleep 30
-python runner.py --seed 102 --numc 4 --widen 4 --relabel 0.2
-sleep 30
-python runner.py --seed 102 --numc 4 --widen 4 --relabel 0.5
-sleep 30
-python runner.py --seed 102 --numc 4 --widen 4 --relabel 0.9
-sleep 30
-python runner.py --seed 112 --numc 4 --widen 4 --relabel 0.0
-sleep 30
-python runner.py --seed 112 --numc 4 --widen 4 --relabel 0.1
-sleep 30
-python runner.py --seed 112 --numc 4 --widen 4 --relabel 0.2
-sleep 30
-python runner.py --seed 112 --numc 4 --widen 4 --relabel 0.5
-sleep 30
-python runner.py --seed 112 --numc 4 --widen 4 --relabel 0.9
-sleep 30
 python runner.py --seed 122 --numc 4 --widen 4 --relabel 0.0
 sleep 30
 python runner.py --seed 122 --numc 4 --widen 4 --relabel 0.1
@@ -98,14 +68,4 @@ sleep 30
 python runner.py --seed 182 --numc 4 --widen 4 --relabel 0.5
 sleep 30
 python runner.py --seed 182 --numc 4 --widen 4 --relabel 0.9
-sleep 30
-python runner.py --seed 192 --numc 4 --widen 4 --relabel 0.0
-sleep 30
-python runner.py --seed 192 --numc 4 --widen 4 --relabel 0.1
-sleep 30
-python runner.py --seed 192 --numc 4 --widen 4 --relabel 0.2
-sleep 30
-python runner.py --seed 192 --numc 4 --widen 4 --relabel 0.5
-sleep 30
-python runner.py --seed 192 --numc 4 --widen 4 --relabel 0.9
 sleep 30
