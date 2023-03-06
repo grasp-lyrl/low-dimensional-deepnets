55546873d3157c2e5564eff9e7339217950bc2ae
Jialin <jmao@sas.upenn.edu>
2023-03-06 02:34:32 +0000

    weighted mds

--- eigvals.py
+++ eigvals.py
@@ -5,30 +5,39 @@ import torch as th
 import time
 from utils import get_idx
 
-fn = "yh_all"
-save_fn = "yh_no_aug"
+fn = "yvh_all_geod"
+save_fn = "yvh_half_weighted"
 root = "/home/ubuntu/ext_vol/inpca/inpca_results_all"
-cond = "aug=='none'" 
+cond = "seed<=45" 
 
 didx = th.load("/home/ubuntu/ext_vol/inpca/inpca_results_all/didx_geod_all.p")
 ii = get_idx(didx, cond)
 th.save(didx.iloc[ii].reset_index(drop=True), os.path.join(root, f"didx_{save_fn}.p"))
 
+weight = np.ones(len(ii))
+weight[-1] = 600
+weight /= weight.sum()
+
 print("loading w")
 start_t = time.time()
 f = h5py.File(os.path.join(root, f"w_{fn}.h5"), "r")
 w = f["w"][ii, :][:, ii]
-# w = w[ii, :][:, ii]
 print("w loaded, t: ", time.time() - start_t)
 start_t = time.time()
 print(w.shape)
 
 print("centering")
 n = w.shape[0]
-dmean = w.mean(0, keepdims=True)
+
+dmean = (w*weight).sum(1, keepdims=True)
 w -= dmean
-w -= w.mean(1, keepdims=True)
+w -= (w*weight[:, None]).sum(0)
 w *= -0.5
+
+# dmean = w.mean(0, keepdims=True)
+# w -= dmean
+# w -= w.mean(1, keepdims=True)
+# w *= -0.5
 print("centered, t: ", time.time() - start_t)
 start_t = time.time()
 
