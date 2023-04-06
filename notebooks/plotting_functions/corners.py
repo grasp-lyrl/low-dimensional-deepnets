5cf691280f1f8e4cd6b0693204d0100ac5c2a522
Jialin <jmao@sas.upenn.edu>
2023-04-06 02:23:58 +0000

    clean up

--- notebooks/plotting_functions/corners.py
+++ notebooks/plotting_functions/corners.py
@@ -3,21 +3,22 @@ from setup import *
 
 @call_parse
 def main(
-      k: Param('k', str, default='yh')
+      k: Param('k', str, default='yh'),
+      fn: Param('fn', str, default='all'),
 ):
-      didx = th.load('/home/ubuntu/ext_vol/inpca/inpca_results_all/corners/didx_test.p')
-      r = th.load(f'/home/ubuntu/ext_vol/inpca/inpca_results_all/corners/r_{k}_test.p')
+      didx = th.load(f'/home/ubuntu/ext_vol/inpca/inpca_results_all/corners/didx_{fn}.p')
 
+      r = th.load(f'/home/ubuntu/ext_vol/inpca/inpca_results_all/corners/r_{k}_{fn}.p')
       fig = plotly_3d(dc=didx.reset_index(drop=True), r=r,
                   opacity=0.5,
-                  color_axis=False,
+                  color_axis=True,
                   # cdict=CDICT_M,
                   ne=5,
                   size=2,
                   color='iseed',
                   colorscale='Set1',
                   legend=True,
-                  cols=['seed', 'iseed', 'm', 'opt', 'err', 'verr', 'corner', 't',
+                  cols=['seed',  'm', 'opt', 'err', 'verr', 't', 
                         'bs', 'aug', 'lr', 'wd'],
                   xrange=[r['xp'][:, 0].min(), r['xp'][:, 0].max()],
                   yrange=[r['xp'][:, 1].min(), r['xp'][:, 1].max()],
